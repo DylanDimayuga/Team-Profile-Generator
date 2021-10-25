b4af1e886cccbd8ed2c62960b129cdd076112c85
Dylan Dimayuga <dylandimayuga@yahoo.com>
2021-10-25 18:45:05 -0500

    added a js file holding all the questions that will be asked in the console.

--- index.js
+++ index.js
@@ -2,3 +2,79 @@ const Employee = require('./Employee')
 const Engineer = require('./Engineer')
 const Intern = require('./Intern')
 const Manager = require('./Manager')
+
+const yourRole = {
+    type: "list",
+    message: "What member would you like to add?",
+    name: "role",
+    choices: ["Intern", "Engineer", "I am finished adding members."]
+}
+
+const Questions = {
+    Manager: [
+        {
+            type: "input",
+            message: "Enter your/their name",
+            name: name,
+        },
+        {
+            type: "input",
+            message: "Enter your/their ID number",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Enter your/their email address",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter your/their office number",
+            name: "office"
+        },
+    ],
+    Engineer: [
+        {
+            type: "input",
+            message: "Enter your/their name",
+            name: name,
+        },
+        {
+            type: "input",
+            message: "Enter your/their ID number",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Enter your/their email address",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter your/their github username",
+            name: "githubUser"
+        },
+    ],
+    Intern: [
+        {
+            type: "input",
+            message: "Enter your/their name",
+            name: name,
+        },
+        {
+            type: "input",
+            message: "Enter your/their ID number",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Enter your/their email address",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter your/their school",
+            name: "school"
+        },
+    ],
+}
