ead838e77aa644bd27f1cb84b33f5e7d26938d7d
Dylan Dimayuga <dylandimayuga@yahoo.com>
2021-10-25 20:01:33 -0500

    Added a generateHTML and generateCSS javascript files containing the markup on how I want them to be created after the user has done inputting all the information they have. Also created created a new readme.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Team-Profile-Generator
-An application that generates an html and css document containing all the created team members and the information that was provided for them.
+
+  
+  
+  # Team-Profile-Generator
+
+  ## Description
+    An application that generates an html and css document containing all the created team members and the information that was provided for them.
+    
+  ## Table Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  Go through my github page and look at repositories where it can be cloned and used on any individuals computer within the terminal.
+  
+  ## Usage
+  This application can be started by first making sure the terminal is within the team-profile-generator directory, then the user will want to run an 'npm i' to ensure that all the dependencies are downloaded onto the computer, and then run 'node index.js' to start the application. Once started the user will just follow through the questions presented in the terminal.
+
+  ## Credits
+  Northwestern University Coding Bootcamp
+
+  ## License
+  This page is covered by an N/A license. Follow the link to the license page.
+  
+  
+
+  ## Questions
+    Github: (https://github.com/kuyadylan)
+    Email: dylandimayuga@yahoo.com
--- index.js
+++ index.js
@@ -1,80 +1,138 @@
-const Employee = require('./Employee')
-const Engineer = require('./Engineer')
-const Intern = require('./Intern')
-const Manager = require('./Manager')
+const inquirer = require('inquirer');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+const Manager = require('./Manager');
 
-const yourRole = {
-    type: "list",
-    message: "What member would you like to add?",
-    name: "role",
-    choices: ["Intern", "Engineer", "I am finished adding members."]
+class questions {
+    constructor() {
+        this.employees = []
+    }
+
+    promptUser() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter what you would like the name of the file to be",
+                name: "fileName"
+            },
+            {
+                type: "input",
+                message: "Enter your name",
+                name: 'name',
+            },
+            {
+                type: "input",
+                message: "Enter your/their ID number",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Enter your/their email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter your/their office number",
+                name: "office",
+            },
+        ])
+        .then(({name, id, email, office}) => {
+            this.employees.push(new Manager(name, id, email, office));
+            this.newRole();
+        })
+    }
+
+    newRole() {
+        inquirer.prompt({
+            type: "list",
+            message: "What member would you like to add?",
+            name: "newRole",
+            choices: ["Engineer", "Intern", "I am finished adding members."],
+        })
+        .then((data) => {
+            if (data.newRole === "Engineer"){
+                this.addEngineer()
+            } else if (data.newRole === "Intern") {
+                this.addIntern();
+            } else {
+                this.writeFile()
+            }
+        })
+    }
+
+    addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter your/their name",
+                name: name,
+            },
+            {
+                type: "input",
+                message: "Enter your/their ID number",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Enter your/their email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter your/their github username",
+                name: "githubUser"
+            },
+        ])
+        .then(({ name, id, email, githubUser }) => {
+            this.employees.push(new Engineer(name, id, email, githubUser))
+            this.newRole
+        })
+    }
+
+    addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter your/their name",
+                name: name,
+            },
+            {
+                type: "input",
+                message: "Enter your/their ID number",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Enter your/their email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter your/their school",
+                name: "school"
+            },
+        ])
+        .then(({name, id, email, school}) => {
+            this.employees.push(new Intern(name, id, email, school))
+            this.newRole
+        })
+    }
+    
+    writeFile() {
+        const fileName = `${data.fileName.toLowerCase().split(' ').join('')}.html`
+        fs.writeFile(fileName, generateHTML(this.employees), (err) => {
+            if (err) {
+                console.log(err)
+            } else
+            console.log('The HTML file has successfully been created')
+        })
+        fs.writeFile(style.css, generateCSS(), (err) => {
+            if (err) {
+                console.log(err)
+            } else
+            console.log('The CSS file has successfully been created.')
+        })
+    }
 }
 
-const Questions = {
-    Manager: [
-        {
-            type: "input",
-            message: "Enter your/their name",
-            name: name,
-        },
-        {
-            type: "input",
-            message: "Enter your/their ID number",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter your/their email address",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Enter your/their office number",
-            name: "office"
-        },
-    ],
-    Engineer: [
-        {
-            type: "input",
-            message: "Enter your/their name",
-            name: name,
-        },
-        {
-            type: "input",
-            message: "Enter your/their ID number",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter your/their email address",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Enter your/their github username",
-            name: "githubUser"
-        },
-    ],
-    Intern: [
-        {
-            type: "input",
-            message: "Enter your/their name",
-            name: name,
-        },
-        {
-            type: "input",
-            message: "Enter your/their ID number",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter your/their email address",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Enter your/their school",
-            name: "school"
-        },
-    ],
-}
+questions()
